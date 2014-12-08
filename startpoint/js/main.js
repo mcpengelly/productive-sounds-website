10b3bacf6079d7081bb09586edfd85db89e3e999
mcpengelly <pengelly.mat@gmail.com>
2014-12-07 21:20:11 -0500

    adding website to git for version control.

---
+++ startpoint/js/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+	$(".navbar li, .container li").hover(function(){
+	  $(this).addClass("active");
+	  },function(){
+	  $(this).removeClass("active");
+	});
+});
+
+ $(function() {
+    $( ".slider" ).slider();
+  });
+
