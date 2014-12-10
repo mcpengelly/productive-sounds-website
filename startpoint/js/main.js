03afff6416a870749588282216e0a6edafd772bb
mcpengelly <pengelly.mat@gmail.com>
2014-12-10 18:37:46 -0500

    music implemented
    music added to site, started adjusting volume controls also.

--- startpoint/js/main.js
+++ startpoint/js/main.js
@@ -5,5 +5,17 @@ $(document).ready(function(){
 	  },function(){
 	  $(this).removeClass("active");
 	});
+	
+	$("#volume-up-button").click(function(){
+		$("#now-playing-audio").volume += 0.1;
+	});
+	$("#volume-down-button").click(function(){
+		$("#now-playing-audio").volume -= 0.1;
+	});
+	$("#mute-button").click(function(){
+		$("#now-playing-audio").mute;
+	});
+	
+
 });
 
