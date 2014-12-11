432d040452a4ebf651bc9b9b40825e5d97984ac4
mcpengelly <pengelly.mat@gmail.com>
2014-12-11 11:14:18 -0500

    implemented audio controls
    audio player for website can now be paused, resumed, stopped, and volume
    can be adjusted.

--- startpoint/js/main.js
+++ startpoint/js/main.js
@@ -1,42 +1,58 @@
 
 $(document).ready(function(){
 
-	var audioPlayer = $("#now-playing-audio");
-	var audioSrc = $("#audio-src");
+	var audioPlayer = document.getElementById('myaudio');
+	var audioSrc = document.getElementById('audio-src');
+	var defaultVolume = 0.5;
+
+
+	if(audioPlayer != null){
+		alert("audio player present")
+		audioPlayer.volume = defaultVolume;
+	}
 	
+	//event handlers below //
 	$(".navbar li, .container li").hover(function(){
-	  $(this).addClass("active");
-	  },function(){
-	  $(this).removeClass("active");
+		$(this).addClass("active");
+		},function(){
+		$(this).removeClass("active");
 	});
 	
-	$("#play-button").click(function(){
-		audioPlayer.play;
-	});
-	
-	$("#pause-button").click(function(){
-		audioPlayer.pause;
-	});
-	
-	$("#mute-button").click(function(){
-		audioPlayer.mute;
-	});
-	
-	$("#volume-up-button").click(function(){
+	//audio button controls //
+	$("#volume-up-button").click(function( ) {
 		audioPlayer.volume += 0.1;
 	});
-	
+
 	$("#volume-down-button").click(function(){
 		audioPlayer.volume -= 0.1;
 	});
+
+	$("#pause-button").click(function(){
+		audioPlayer.pause();
+	});
 	
+	$("#play-button").click(function(){
+		audioPlayer.play();
+	});
+
+	$("#mute-button").click(function(){
+		audioPlayer.mute();
+	});
+
+	$("#stop-button").click(function(){
+		audioPlayer.pause();
+		audioPlayer.currentTime = 0;
+	});
+
+	
+	//not functional atm
 	$("#song-1-button").click(function(){
-		audioSrc.html('id="audio-src" src="audio/track-1.mp3" type="audio/mpeg"');
-	
+		audioSrc.src = "audio/track-1.mp3";
 	});
+
 	$("#song-2-button").click(function(){
-		$("#audio-src").html('id="audio-src" src="audio/track-2.mp3" type="audio/mpeg"');
-	
+		audioSrc.src = "audio/track-2.mp3";
 	});
+
 });
 
