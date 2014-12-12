4b0f245badbbb362a0d4c98e0b09d794c0bddb1f
mcpengelly <pengelly.mat@gmail.com>
2014-12-12 10:57:34 -0500

    Implemented mute, volume ui and new buttons
    new button controls, front facing volume display, mute button, lights
    off button, + minor bug fixes

--- startpoint/js/main.js
+++ startpoint/js/main.js
@@ -3,14 +3,17 @@ $(document).ready(function(){
 
 	var audioPlayer = document.getElementById("myaudio");
 	var audioSrc = document.getElementById('audio-src');
-	var defaultVolume = 0.5;
+	var defaultVolume = 0.3;
+	var lights = true; //lights start on.
 
 	
-	if(audioPlayer != null){
-		audioPlayer.load();
-		audioPlayer.volume = defaultVolume;	
-		audioPlayer.play();
-		alert("audio player initialized")
+	if(audioPlayer !== null){
+	 audioPlayer.load();
+	 audioPlayer.volume = defaultVolume;	
+	 audioPlayer.play();
+	 alert("audio player initialized");
+	 $( "#now-playing" ).text( "Now Playing: Track 1");
+	 $( "#current-volume" ).text( "Volume: " + audioPlayer.volume * 10);
 		
 	}
 	
@@ -26,15 +29,28 @@ $(document).ready(function(){
 		$("#about-content").addClass("hide");
 		$("#landing-content").removeClass("hide");
 	});
+	
 	$("#about-button").click(function( ) {
 		$("#landing-content").addClass("hide");
 		$("#about-content").removeClass("hide");
 	});
+	
 	$("#contact-button").click(function( ) {
 
 	});
+	
 	$("#lights-button").click(function( ) {
-		
+		// change background to black and dim page content
+		if(lights === true){
+			$("#surround-this").addClass("darken");
+			$(".jumbotron").addClass("dim");
+			lights = false; // set ligths flag to off
+		}
+		else{
+			$("#surround-this").removeClass("darken");
+			$(".jumbotron").removeClass("dim");
+			lights = true; //set lights flag to on
+		}
 	});
 	$("#pomodoro-button").click(function( ) {
 		
@@ -46,10 +62,14 @@ $(document).ready(function(){
 	//audio button controls //
 	$("#volume-up-button").click(function( ) {
 		audioPlayer.volume += 0.1;
+		//display volume * 10 as a simple volume indicator
+		$( "#current-volume" ).text( "Volume: " + Math.round(audioPlayer.volume * 10));
 	});
 
 	$("#volume-down-button").click(function(){
 		audioPlayer.volume -= 0.1;
+		//display volume * 10 as a simple volume indicator
+		$( "#current-volume" ).text( "Volume: " + Math.round(audioPlayer.volume * 10));
 	});
 
 	$("#pause-button").click(function(){
@@ -61,7 +81,15 @@ $(document).ready(function(){
 	});
 
 	$("#mute-button").click(function(){
-		audioPlayer.mute();
+	//if audio is not muted when clicked, mute it
+		if(audioPlayer.muted === false){
+			audioPlayer.muted = true;
+			//add indication that were muted
+		}
+		else{ // else unmute it
+			audioPlayer.muted = false;
+		}	
+		
 	});
 
 	$("#stop-button").click(function(){
@@ -81,10 +109,7 @@ $(document).ready(function(){
 	});
 
 	$("#song-2-button").click(function(){
-		audioPlayer.pause();
-		audioSrc.attr('src', song2);
-		audioSrc.load();
-		audioPlayer.play();
+
 	});
 
 });
